ce1e5d02ea3222dc019d805faa0f86f0701b4715
YSAY <570260087@qq.com>
2022-07-28 19:45:23 +0800

    first commit

---
+++ caculator_body.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  caculator_body.c
+ *
+ *    Description: the body of caculator 
+ *
+ *        Version:  1.0
+ *        Created:  2022年07月27日 18时23分47秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YSAY (DYH), 570260087@qq.com
+ *   Organization:  csu
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <ctype.h>
+void caculator_body(){
+		printf ( "*****************************\n" );
+		printf ( "Enter the statement:\n" );
+		printf ( "*****************************\n" );
+		
+		
+}
